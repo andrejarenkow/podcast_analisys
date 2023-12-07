1afb6bdbd5900a56f07a1381bf63469f226d46bb
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2023-12-06 23:54:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -111,5 +111,10 @@ st.dataframe(dados.set_index('release_date'),
                     
 
                 })
+st.divider()
+st.subheader('Bolão The Game Awards 2023!')
+dados_game_awards = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vTviyi86G1qxhCZacjpN1v8ShugrnPn2Y-WwzcVjpEhNsZCWNcVQAMAOLXwYnj8g_1_IsPx7YMxKr2O/pub?gid=1743518019&single=true&output=csv')
+st.dataframe(dados_game_awards, hide_index=True)
+
 
 
